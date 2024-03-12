e6f84ef02db6569e7d027a3446b98a32ec239761
Mohammed L Shnawy <elshenawy.mohammed7@gmail.com>
2024-03-12 05:44:02 +0200

    settings design

--- assets/js/settings.js
+++ assets/js/settings.js
@@ -1,6 +1,44 @@
-let settings = document.createElement('div');
-settings.id = 'settings';
-
 function settings() {
+    let settings = document.createElement('div');
+    settings.id = 'settings';
+
+    settings.innerHTML = '<div id="closebtndiv"><button id="closebtn" onclick="closeit()">✖</button></div>'
+    + `<section id="ren">
+    <h3 id="stitle">تحديد وقت المؤقت<h3>
     
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    </section>`
+
+
+
+
+
+
+
+
+    document.body.appendChild(settings)
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
+
+function closeit() {document.getElementById('settings').remove()}
