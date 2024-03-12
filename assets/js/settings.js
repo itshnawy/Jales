675f0cacb475e9c21d5aa5bea58e37d0d248871b
Mohammed L Shnawy <elshenawy.mohammed7@gmail.com>
2024-03-12 07:07:12 +0200

    edit some errors on changetimer function

--- assets/js/settings.js
+++ assets/js/settings.js
@@ -1,28 +1,7 @@
 const timeSpan = document.querySelector("div#timer span");
 const scriptt = document.createElement("script");
 
-function twnyfvmin() {
-    setCookie('timerspan', "25:00" , 365 * 10)
-    setCookie('timer', 24.9 ,365 * 10)
-    location.reload();
-
-};
-function twnymin() {
-    setCookie('timerspan', "20:00" , 365 * 10)
-    setCookie('timer', 19.9 ,365 * 10)
-    location.reload();
-
-};
-function fifmin() {
-    setCookie('timerspan', "15:00" , 365 * 10)
-    setCookie('timer', 14.9 ,365 * 10)
-    location.reload();
-};
-function tenmin() {
-    setCookie('timerspan', "10:00" , 365 * 10)
-    setCookie('timer', 9.9 , 365 * 10)
-    location.reload();
-};
+function changeTimer(timer,timerspan) {setCookie('timerspan', timerspan , 365 * 10);setCookie('timer', timer ,365 * 10);location.reload(); }
 
 function settings() {
     let settings = document.createElement('div');
@@ -37,11 +16,12 @@ function settings() {
     + `<section id="ren">
     <h3 id="stitle">تحديد وقت المؤقت</h3>
     <div class="chooseTimerTime">
-    <button class="timertime" onclick="tenmin()"><i class="fa-solid fa-hourglass-half"></i> <p>10 دقيقة</p></button>
-    <button class="timertime" onclick="fifmin()"><i class="fa-solid fa-hourglass-half"></i> <p>15 دقيقة</p></button>
-    <button class="timertime" onclick="twnymin()"><i class="fa-solid fa-hourglass-half"></i> <p>20 دقيقة</p></button>
-    <button class="timertime" onclick="twnyfvmin()"><i class="fa-solid fa-hourglass-half"></i> <p>25 دقيقة</p></button>
+    <button class="timertime" onclick="changeTimer('9.9','10:00')"><i class="fa-solid fa-hourglass-half"></i> <p>10 دقيقة</p></button>
+    <button class="timertime" onclick="changeTimer('14.9','15:00')"><i class="fa-solid fa-hourglass-half"></i> <p>15 دقيقة</p></button>
+    <button class="timertime" onclick="changeTimer('19.9','20:00')"><i class="fa-solid fa-hourglass-half"></i> <p>20 دقيقة</p></button>
+    <button class="timertime" onclick="changeTimer('24.9','25:00')"><i class="fa-solid fa-hourglass-half"></i> <p>25 دقيقة</p></button>
     </div>
+    
     <h3 id="stitle">تحديد القارئ</h3>
     قريبا
     
