5bdee5ea076ef650f9a9a4dd387e9261a9db88f0
Mohammed L Shnawy <elshenawy.mohammed7@gmail.com>
2024-03-26 04:47:43 +0200

    Update chooseSurah.js

--- assets/js/chooseSurah.js
+++ assets/js/chooseSurah.js
@@ -27,11 +27,7 @@ function choosetheSurah() {
         let chc = getCookie('Qareaa');
         const audioSrc = item[chc] !== undefined ? item[chc] : item['src'];
         let content = "<div class='SurahCardSelection' data-audio-src='" + audioSrc + "' data-name='" + "سورة " + sname + "'>"  + "سورة " + sname +'<i class="fa-solid fa-add" style="color: #01ACEF;background: #0000002b;padding: 4px 5px;border-radius: 10px;font-size: 13px;"></i>'+ "</div>";
-        row.innerHTML += content;
-
-
-        const audio = new Audio(audioSrc);
-        audio.preload = 'auto';
+        row.innerHTML += content;        
 
       });
       let overlay = document.createElement('div');
